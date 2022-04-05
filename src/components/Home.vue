12791125632eba900f9beabd35b5693b8551cea5
lin2333 <2567165156@qq.com>
2022-04-05 22:03:17 +0800

    home退出按钮

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,13 +1,20 @@
 <template>
     <div>
-        Home
+        <el-button type="info" @click="logout">退出</el-button>
     </div>
 </template>
 <script>
-
+import { useRouter } from 'vue-router'
 export default ({
     setup() {
-        
+        const router = useRouter()
+        function logout() {
+            window.sessionStorage.clear();
+            router.push('/login');
+        }
+        return{
+            logout
+        }
     },
 })
 </script>
